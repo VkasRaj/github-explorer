11debc45667a46559fe6a7ecac85297b00f64c51
vkasraj <vikas@klickinvite.com>
2018-08-03 19:00:11 +0530

    models: added user model

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minlength: 6,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6,
+		trim: true
+	}
+});
+
+module.exports = mongoose.model("User", User);
