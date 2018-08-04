01fcf79a21527fdf16ff5bf5d22371fb243fb943
vkasraj <vikas@klickinvite.com>
2018-08-04 16:32:10 +0530

    route: added search route

--- client/src/root/Routes/Routes.js
+++ client/src/root/Routes/Routes.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Home from "../../views/Home";
 import Signup from "../../views/Signup";
 import Login from "../../views/Login";
+import Search from "../../views/Search";
 
 class Routes extends Component {
 	render() {
 		return (
 			<Switch>
+				<Route path="/search" component={Search} />
 				<Route path="/login" component={Login} />
 				<Route path="/signup" component={Signup} />
 				<Route exact path="/" component={Home} />
+				<Redirect to="/" />
 			</Switch>
 		);
 	}
