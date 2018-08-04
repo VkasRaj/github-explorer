8e793bc2727d31e14d8ed2d60e9a8ec2a6631733
vkasraj <vikas@klickinvite.com>
2018-08-04 13:47:19 +0530

    client: added store and router

--- client/src/root/App.js
+++ client/src/root/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import reducers from "../store/reducers/index";
+import Routes from "./Routes/Routes";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const REDUCER = combineReducers(reducers);
+
+const STORE = createStore(REDUCER, composeEnhancers(applyMiddleware(thunk)));
 
 class App extends Component {
 	render() {
-		return <div>hello</div>;
+		return (
+			<Provider store={STORE}>
+				<BrowserRouter>
+					<Routes />
+				</BrowserRouter>
+			</Provider>
+		);
 	}
 }
 
