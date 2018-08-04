cbee21de032b0ef3f2767d7ba10a306741fa8388
vkasraj <vikas@klickinvite.com>
2018-08-04 17:00:05 +0530

    routes: added private and public route

--- client/src/root/Routes/Routes.js
+++ client/src/root/Routes/Routes.js
@@ -1,33 +1,42 @@
 import React, { Component } from "react";
-import { withRouter, Switch, Route, Redirect } from "react-router-dom";
+import { withRouter, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Home from "../../views/Home";
 import Signup from "../../views/Signup";
 import Login from "../../views/Login";
 import Search from "../../views/Search";
+
+import Private from "./config/PrivateRoute";
+import Public from "./config/PublicRoute";
+
 import { autoSignIn } from "../../store/actions/index";
 
 class Routes extends Component {
 	componentDidMount() {
 		const { autoSignIn } = this.props;
-		console.log(1);
 
 		autoSignIn();
 	}
 
 	render() {
+		const { isAuth } = this.props;
+
 		return (
 			<Switch>
-				<Route path="/search" component={Search} />
-				<Route path="/login" component={Login} />
-				<Route path="/signup" component={Signup} />
-				<Route exact path="/" component={Home} />
+				<Private isAuth={isAuth} path="/search" component={Search} />
+				<Public isAuth={isAuth} path="/login" component={Login} />
+				<Public isAuth={isAuth} path="/signup" component={Signup} />
+				<Public isAuth={isAuth} exact path="/" component={Home} />
 				<Redirect to="/" />
 			</Switch>
 		);
 	}
 }
+
+const mapStateToProps = state => ({
+	isAuth: state.user.user ? true : false
+});
 
 const mapDispatchToProps = dispatch => ({
 	autoSignIn: () => dispatch(autoSignIn())
@@ -35,7 +44,7 @@ const mapDispatchToProps = dispatch => ({
 
 export default withRouter(
 	connect(
-		null,
+		mapStateToProps,
 		mapDispatchToProps
 	)(Routes)
 );
