ef8a13c59910a00b3372c2b51121fe7515c165b4
vkasraj <vikasraj1911@gmail.com>
2018-08-05 12:04:33 +0530

    forms: added alternate forms links

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import { Typography } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+
 import FormLayout from "../components/Forms/FormLayout";
 import LoginContainer from "../containers/Login";
 
 class Login extends Component {
-	render() {
-		return (
-			<FormLayout>
-				<LoginContainer />
-			</FormLayout>
-		);
-	}
+    render() {
+        return (
+            <FormLayout>
+                <LoginContainer />
+                <Typography
+                    variant="caption"
+                    align="center"
+                    style={{ marginTop: "1rem" }}
+                >
+                    Not have an account? <NavLink to="/signup">Signup</NavLink>
+                </Typography>
+            </FormLayout>
+        );
+    }
 }
 
 export default Login;
--- client/src/views/Signup.js
+++ client/src/views/Signup.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import { Typography } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+
 import FormLayout from "../components/Forms/FormLayout";
 import SignupContainer from "../containers/Signup";
 
 class Signup extends Component {
-	render() {
-		return (
-			<FormLayout>
-				<SignupContainer />
-			</FormLayout>
-		);
-	}
+    render() {
+        return (
+            <FormLayout>
+                <SignupContainer />
+                <Typography
+                    variant="caption"
+                    align="center"
+                    style={{ marginTop: "1rem" }}
+                >
+                    Have an account? <NavLink to="/login">Login</NavLink>
+                </Typography>
+            </FormLayout>
+        );
+    }
 }
 
 export default Signup;
