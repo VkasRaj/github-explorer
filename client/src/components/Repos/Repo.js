443718f0f9dd67e1bf8771ef7c5edeea325a9094
vkasraj <vikasraj1911@gmail.com>
2018-08-05 16:03:56 +0530

    repo: using purecomponent

--- client/src/components/Repos/Repo.js
+++ client/src/components/Repos/Repo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import {
     Grid,
     Typography,
@@ -31,111 +31,142 @@ const styles = theme => ({
     }
 });
 
-const Repo = ({ repo, classes: { icon, linkMargin, cardMargin } }) => {
-    let homepageLink = repo.homepage && (
-        <Grid container alignItems="center" className={linkMargin}>
-            <WebIcon className={icon} />
-            <Typography variant="caption">
-                <a
-                    href={repo.homepage}
-                    target="_blank"
-                    style={{ color: "inherit" }}
-                >
-                    {repo.homepage}
-                </a>
-            </Typography>
-        </Grid>
-    );
+class Repo extends PureComponent {
+    render() {
+        const {
+            repo,
+            classes: { icon, linkMargin, cardMargin }
+        } = this.props;
 
-    return (
-        <Grid container justify="center" className={cardMargin}>
-            <Grid item xs={11} sm={8} md={6}>
-                <Card>
-                    <Grid container>
-                        <Hidden smDown>
-                            <Grid item xs={12} md={3}>
-                                <CardMedia
-                                    style={{ height: "100%" }}
-                                    alt="github repo owner avatar"
-                                    title={repo.owner.login}
-                                    image={repo.owner.avatar_url}
-                                />
-                            </Grid>
-                        </Hidden>
-                        <Grid item xs={12} md={9}>
-                            <CardContent>
-                                <Typography>{repo.full_name}</Typography>
-                                <Typography variant="caption" gutterBottom>
-                                    {repo.description}
-                                </Typography>
-                                <Grid container justify="space-between">
-                                    <Grid item>
-                                        <Typography variant="caption" paragraph>
-                                            Created: {repo.created_at}
-                                        </Typography>
-                                    </Grid>
-                                    <Grid>
-                                        <Typography variant="caption" paragraph>
-                                            Size: {Math.floor(repo.size / 1024)}MB
-                                        </Typography>
-                                    </Grid>
-                                </Grid>
-                                <Grid
-                                    container
-                                    alignItems="center"
-                                    className={!repo.homepage ? linkMargin : ""}
-                                >
-                                    <LinkIcon className={icon} />
-                                    <Typography variant="caption">
-                                        <a
-                                            href={repo.html_url}
-                                            target="_blank"
-                                            style={{ color: "inherit" }}
-                                        >
-                                            {repo.html_url}
-                                        </a>
-                                    </Typography>
-                                </Grid>
-                                {homepageLink}
-                                <Grid container justify="space-around">
-                                    <Grid item>
-                                        <Tooltip title="Stars">
-                                            <Grid container alignItems="center">
-                                                <StarIcon className={icon} />
-                                                <Typography variant="caption">
-                                                    {repo.stargazers_count}
-                                                </Typography>
-                                            </Grid>
-                                        </Tooltip>
-                                    </Grid>
-                                    <Grid item>
-                                        <Tooltip title="Forks">
-                                            <Grid container alignItems="center">
-                                                <ForkIcon className={icon} />
-                                                <Typography variant="caption">
-                                                    {repo.forks_count}
-                                                </Typography>
-                                            </Grid>
-                                        </Tooltip>
-                                    </Grid>
-                                    <Grid item>
-                                        <Tooltip title="Open Issues">
-                                            <Grid container alignItems="center">
-                                                <IssueIcon className={icon} />
-                                                <Typography variant="caption">
-                                                    {repo.open_issues_count}
-                                                </Typography>
-                                            </Grid>
-                                        </Tooltip>
-                                    </Grid>
-                                </Grid>
-                            </CardContent>
-                        </Grid>
-                    </Grid>
-                </Card>
+        let homepageLink = repo.homepage && (
+            <Grid container alignItems="center" className={linkMargin}>
+                <WebIcon className={icon} />
+                <Typography variant="caption">
+                    <a
+                        href={repo.homepage}
+                        target="_blank"
+                        style={{ color: "inherit" }}
+                    >
+                        {repo.homepage}
+                    </a>
+                </Typography>
             </Grid>
-        </Grid>
-    );
-};
+        );
+
+        return (
+            <Grid container justify="center" className={cardMargin}>
+                <Grid item xs={11} sm={8} md={6}>
+                    <Card>
+                        <Grid container>
+                            <Hidden smDown>
+                                <Grid item xs={12} md={3}>
+                                    <CardMedia
+                                        style={{ height: "100%" }}
+                                        alt="github repo owner avatar"
+                                        title={repo.owner.login}
+                                        image={repo.owner.avatar_url}
+                                    />
+                                </Grid>
+                            </Hidden>
+                            <Grid item xs={12} md={9}>
+                                <CardContent>
+                                    <Typography>{repo.full_name}</Typography>
+                                    <Typography variant="caption" gutterBottom>
+                                        {repo.description}
+                                    </Typography>
+                                    <Grid container justify="space-between">
+                                        <Grid item>
+                                            <Typography
+                                                variant="caption"
+                                                paragraph
+                                            >
+                                                Created: {repo.created_at}
+                                            </Typography>
+                                        </Grid>
+                                        <Grid>
+                                            <Typography
+                                                variant="caption"
+                                                paragraph
+                                            >
+                                                Size:{" "}
+                                                {Math.floor(repo.size / 1024)}MB
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid
+                                        container
+                                        alignItems="center"
+                                        className={
+                                            !repo.homepage ? linkMargin : ""
+                                        }
+                                    >
+                                        <LinkIcon className={icon} />
+                                        <Typography variant="caption">
+                                            <a
+                                                href={repo.html_url}
+                                                target="_blank"
+                                                style={{ color: "inherit" }}
+                                            >
+                                                {repo.html_url}
+                                            </a>
+                                        </Typography>
+                                    </Grid>
+                                    {homepageLink}
+                                    <Grid container justify="space-around">
+                                        <Grid item>
+                                            <Tooltip title="Stars">
+                                                <Grid
+                                                    container
+                                                    alignItems="center"
+                                                >
+                                                    <StarIcon
+                                                        className={icon}
+                                                    />
+                                                    <Typography variant="caption">
+                                                        {repo.stargazers_count}
+                                                    </Typography>
+                                                </Grid>
+                                            </Tooltip>
+                                        </Grid>
+                                        <Grid item>
+                                            <Tooltip title="Forks">
+                                                <Grid
+                                                    container
+                                                    alignItems="center"
+                                                >
+                                                    <ForkIcon
+                                                        className={icon}
+                                                    />
+                                                    <Typography variant="caption">
+                                                        {repo.forks_count}
+                                                    </Typography>
+                                                </Grid>
+                                            </Tooltip>
+                                        </Grid>
+                                        <Grid item>
+                                            <Tooltip title="Open Issues">
+                                                <Grid
+                                                    container
+                                                    alignItems="center"
+                                                >
+                                                    <IssueIcon
+                                                        className={icon}
+                                                    />
+                                                    <Typography variant="caption">
+                                                        {repo.open_issues_count}
+                                                    </Typography>
+                                                </Grid>
+                                            </Tooltip>
+                                        </Grid>
+                                    </Grid>
+                                </CardContent>
+                            </Grid>
+                        </Grid>
+                    </Card>
+                </Grid>
+            </Grid>
+        );
+    }
+}
 
 export default withStyles(styles)(Repo);
