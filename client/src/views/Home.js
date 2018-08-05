0170bffcc7d9c4fcc9c768a93945ea1b5bf220c5
vkasraj <vikasraj1911@gmail.com>
2018-08-05 16:43:01 +0530

    Home: added login signup buttons

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
+import { Button, Typography } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+
+import FormLayout from "../components/Forms/FormLayout";
 
 class Home extends Component {
-	render() {
-		return <div>Hello</div>;
-	}
+    render() {
+        return (
+            <FormLayout>
+                <Typography paragraph>
+                    <Button
+                        fullWidth
+                        color="primary"
+                        variant="contained"
+                        component={NavLink}
+                        to="/login"
+                    >
+                        Login
+                    </Button>
+                </Typography>
+                <Typography paragraph>
+                    <Button
+                        fullWidth
+                        variant="outlined"
+                        component={NavLink}
+                        to="/signup"
+                    >
+                        Signup
+                    </Button>
+                </Typography>
+            </FormLayout>
+        );
+    }
 }
 
 export default Home;
