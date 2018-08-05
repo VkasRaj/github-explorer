d24ed9aaabf1c0f7a0ab9b60b44cdb3e3ae22f5c
vkasraj <vikasraj1911@gmail.com>
2018-08-05 15:46:03 +0530

    Repos: added loader

--- client/src/containers/Repos.js
+++ client/src/containers/Repos.js
@@ -1,18 +1,38 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
+import { asyncComponent } from "react-async-component";
+import { Grid, LinearProgress } from "@material-ui/core";
 
-import ReposList from "../components/Repos/Repos";
+const ReposList = asyncComponent({
+    resolve: () => import("../components/Repos/Repos")
+});
 
 class Repos extends Component {
     render() {
-        const { repos } = this.props;
+        const { repos, reposLoading } = this.props;
 
-        return repos && <ReposList repos={repos} />;
+        return (
+            <Fragment>
+                {reposLoading && (
+                    <Grid
+                        container
+                        justify="center"
+                        style={{ marginBottom: "1rem" }}
+                    >
+                        <Grid item xs={10} sm={6} md={3}>
+                            <LinearProgress />
+                        </Grid>
+                    </Grid>
+                )}
+                {repos && <ReposList repos={repos} />}
+            </Fragment>
+        );
     }
 }
 
 const mapStateToProps = state => ({
-    repos: state.github.repos
+    repos: state.github.repos,
+    reposLoading: state.github.loading
 });
 
 export default connect(mapStateToProps)(Repos);
