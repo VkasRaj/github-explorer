bc822a5e09da0c8e35eced8ba16912b43565e7fa
vkasraj <vikasraj1911@gmail.com>
2018-08-07 18:49:26 +0530

    Last Search: fixed showing no model when 0 result

--- client/src/components/LastSearch.js
+++ client/src/components/LastSearch.js
@@ -2,13 +2,19 @@ import React from "react";
 import { Dialog, List, ListItem, ListItemText } from "@material-ui/core";
 
 const LastSearch = ({ searchList, open, close }) => {
-    const _list =
-        searchList &&
-        searchList.map((search, $i) => (
+    let _list = (
+        <ListItem>
+            <ListItemText primary="No search entry found" />
+        </ListItem>
+    );
+
+    if (searchList && searchList.length) {
+        _list = searchList.map((search, $i) => (
             <ListItem key={search._id}>
                 <ListItemText primary={`${$i + 1}. ${search.query}`} />
             </ListItem>
         ));
+    }
 
     return (
         <Dialog open={Boolean(open)} onClose={close}>
