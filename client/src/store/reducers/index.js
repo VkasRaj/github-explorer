97e773ac7a86bea201e2c466401c88bce27fa342
vkasraj <vikasraj1911@gmail.com>
2018-08-09 20:32:00 +0530

    store: removed redux-form reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,10 +1,7 @@
-import { reducer as formReducer } from "redux-form";
-
 import user from "./user";
 import search from "./search";
 
 export default {
-    form: formReducer,
     user,
     github: search
 };
