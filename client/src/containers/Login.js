458de7370611e7325f1658633127e07701ab4470
vkasraj <vikasraj1911@gmail.com>
2018-08-12 12:04:06 +0530

    Login: fixed error props

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -5,32 +5,37 @@ import LoginForm from "../components/Forms/Login";
 import { login } from "../store/actions";
 
 class Login extends Component {
-    onLogin = values => {
-        const { userLogin } = this.props;
+    state = {
+        error: null
+    };
 
-        userLogin(values, () => {
-            console.log("[Redirect]");
+    onLogin = values => {
+        const {
+            state: { error },
+            props: { userLogin }
+        } = this;
+
+        error && this.setState({ error: null });
+
+        userLogin(values, error => {
+            error && this.setState({ error });
         });
     };
 
     render() {
         const {
             onLogin,
-            props: { error }
+            state: { error }
         } = this;
         return <LoginForm onSubmit={onLogin} formError={error} />;
     }
 }
-
-const mapStateToProps = state => ({
-    error: state.user.error.login
-});
 
 const mapDispatchToProps = dispatch => ({
     userLogin: (v, cb) => dispatch(login(v, cb))
 });
 
 export default connect(
-    mapStateToProps,
+    null,
     mapDispatchToProps
 )(Login);
