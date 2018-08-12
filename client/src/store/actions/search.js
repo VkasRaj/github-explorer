5a6c24d4d285be7e53084fd6738ed6f6522c3396
vkasraj <vikasraj1911@gmail.com>
2018-08-12 12:47:55 +0530

    Store: removed extra actions

--- client/src/store/actions/search.js
+++ client/src/store/actions/search.js
@@ -1,30 +1,20 @@
 import axios from "axios";
 
-import {
-    FETCH_REPO_START,
-    FETCH_REPO_SUCCESS,
-    FETCH_REPO_FAIL,
-    FETCH_SEARCH_LIST
-} from "./actionTypes";
-
-const repoStart = () => ({ type: FETCH_REPO_START });
+import { FETCH_REPO_SUCCESS, FETCH_SEARCH_LIST } from "./actionTypes";
 
 const repoSuccess = repos => ({ type: FETCH_REPO_SUCCESS, repos });
-
-const repoFail = error => ({ type: FETCH_REPO_FAIL, error });
 
 const searchList = list => ({ type: FETCH_SEARCH_LIST, list });
 
 export const search = ({ search }) => {
     return dispatch => {
-        dispatch(repoStart());
         axios
             .post("/api/user/search", { search })
             .then(({ data: { items } }) => {
                 dispatch(repoSuccess(items));
             })
             .catch(error => {
-                dispatch(repoFail(error));
+                throw error;
             });
     };
 };
@@ -37,7 +27,7 @@ export const onSearchList = () => {
                 dispatch(searchList(result));
             })
             .catch(error => {
-                dispatch(repoFail(error));
+                throw error;
             });
     };
 };
--- client/src/store/reducers/search.js
+++ client/src/store/reducers/search.js
@@ -1,38 +1,21 @@
-import {
-    FETCH_REPO_START,
-    FETCH_REPO_SUCCESS,
-    FETCH_REPO_FAIL,
-    FETCH_SEARCH_LIST
-} from "../actions/actionTypes";
+import { FETCH_REPO_SUCCESS, FETCH_SEARCH_LIST } from "../actions/actionTypes";
 
 const intiState = {
     repos: null,
-    loading: false,
-    error: null,
     lastSearch: null
 };
 
-const repoStart = (state, action) => ({ ...state, loading: true, error: null });
-
 const repoSuccess = (state, { repos }) => ({
     ...state,
-    loading: false,
-    repos,
-    error: null
+    repos
 });
-
-const repoFail = (state, { error }) => ({ ...state, loading: false, error });
 
 const searchList = (state, { list }) => ({ ...state, lastSearch: list });
 
 const reducer = (state = intiState, { type, ...action }) => {
     switch (type) {
-        case FETCH_REPO_START:
-            return repoStart(state, action);
         case FETCH_REPO_SUCCESS:
             return repoSuccess(state, action);
-        case FETCH_REPO_FAIL:
-            return repoFail(state, action);
         case FETCH_SEARCH_LIST:
             return searchList(state, action);
         default:
